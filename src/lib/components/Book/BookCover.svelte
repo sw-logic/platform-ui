ea8a47d89f0bfe7999ed1735876b4ccf6448109c
zsolt <doczi.zsolt@smartworks.studio>
2025-08-07 16:34:50 +0200

    fix book images

--- src/lib/components/Book/BookCover.svelte
+++ src/lib/components/Book/BookCover.svelte
@@ -3,11 +3,13 @@
 	export let cover = 'book-1.webp';
 	export let type = 'book';
 	export let tag = '';
+
+	console.log('base', base);
 </script>
 
 
 <picture class="book {type}">
-	<img src="{base}/images/book/{cover}" alt="Name of the book">
+	<img src="/images/book/{cover}" alt="Name of the book">
 	{#if tag}
 		<span class="book-tag">{tag}</span>
 	{/if}
