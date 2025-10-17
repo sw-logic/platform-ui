fc843413e911da9201720ccb0f89fa1c61fdb729
Swoopy <swoopy@smartworks.studio>
2025-10-17 15:22:23 +0200

    - icon active state

--- src/lib/components/Documents/ParaTools.svelte
+++ src/lib/components/Documents/ParaTools.svelte
@@ -6,8 +6,8 @@
 
 <div class="para-tools">
 	<div class="para-buttons">
-		<ControlIcon class="btn-bookmark" icon="bookmark" />
-		<ControlIcon class="btn-note" icon="chat-box" data-bs-toggle="collapse" data-bs-target="#para-note-{id}" aria-expanded="false" aria-controls="collapseExample" />
-		<ControlIcon class="btn-refs" icon="quote" data-bs-toggle="collapse" data-bs-target="#para-refs-{id}" aria-expanded="false" aria-controls="collapseExample" />
+		<ControlIcon class="btn-bookmark" icon="bookmark" name="bookmark" />
+		<ControlIcon class="btn-note" icon="chat-box" name="note" data-bs-toggle="collapse" data-bs-target="#para-note-{id}" aria-expanded="false" aria-controls="collapseExample" />
+		<ControlIcon class="btn-refs" icon="quote" name="refs" data-bs-toggle="collapse" data-bs-target="#para-refs-{id}" aria-expanded="false" aria-controls="collapseExample" />
 	</div>
 </div>
