fcc05fab623a57bdf2d55dc706b39ef431e3b34e
Swoopy <swoopy@smartworks.studio>
2025-11-03 15:22:40 +0100

    - favorites layout

--- src/routes/(default)/favorites/+page.svelte
+++ src/routes/(default)/favorites/+page.svelte
@@ -1,13 +1,95 @@
 <script>
 	import Icon from '$lib/components/Icon.svelte';
 	import { settings } from '$lib';
+	import { getItems } from '$lib/scripts/getItems.js';
+	import { ActiveToggle } from '$lib/scripts/activeToggle';
+	import Item from '$lib/components/Item/Item.svelte';
+	import FilterBar from '$lib/components/Filters/FilterBar.svelte';
+	import { onMount } from 'svelte';
+
+	const doc_items = getItems(6, { type: 'doc' });
+	const img_items = getItems(6, { type: 'image' });
+
+	onMount(() => {
+		ActiveToggle.init();
+	});
 </script>
 
 <div class="d-flex flex-column flex-grow-1">
 	{#if $settings.userLoggedIn === true}
 		{#if $settings.userHasContent}
 			<div class="container py-5">
-				Favorites...
+				<div class="flex-row-center flex-wrap justify-content-between mb-3">
+					<h1 class="col-12 col-sm-6">My favorites</h1>
+					<FilterBar />
+				</div>
+				<div class="tags tags-rounded my-4" data-active-toggle=".tag">
+					<span>Filter: </span>
+					<a href="#" class="tag active active-bg-primary" data-target="#favoriteDocuments" data-target-class="show">Documents</a>
+					<a href="#" class="tag active active-bg-primary" data-target="#favoriteImages" data-target-class="show">Images</a>
+					<a href="#" class="tag active active-bg-primary" data-target="#favoriteArchives" data-target-class="show">Archives</a>
+				</div>
+
+				<section>
+					<h2>Documents</h2>
+					<div class="mt-4 items item-grid item-grid-6 collapse show" id="favoriteDocuments">
+						{#each doc_items as item, index (index)}
+							<Item
+								type={item.type}
+								cover={item.cover}
+								author={item.author}
+								title={item.title}
+								subtitle={item.subtitle}
+								tag={item.tag}
+								categories={item.categories}
+								access={item.access}
+								trial={item.trial}
+								search={true}
+							/>
+						{/each}
+					</div>
+				</section>
+				<hr class="my-5" />
+				<section>
+					<h2>Images</h2>
+					<div class="mt-4 items item-grid item-grid-6 collapse show" id="favoriteImages">
+						{#each img_items as item, index (index)}
+							<Item
+								type={item.type}
+								cover={item.cover}
+								author={item.author}
+								title={item.title}
+								subtitle={item.subtitle}
+								tag={item.tag}
+								categories={item.categories}
+								access={item.access}
+								trial={item.trial}
+								search={true}
+							/>
+						{/each}
+					</div>
+				</section>
+				<hr class="my-5" />
+				<section>
+					<h2>Archives</h2>
+					<div class="mt-4 items item-grid item-grid-6 collapse show" id="favoriteArchives">
+						{#each doc_items as item, index (index)}
+							<Item
+								type={item.type}
+								cover={item.cover}
+								author={item.author}
+								title={item.title}
+								subtitle={item.subtitle}
+								tag={item.tag}
+								categories={item.categories}
+								access={item.access}
+								trial={item.trial}
+								search={true}
+							/>
+						{/each}
+					</div>
+				</section>
+
 			</div>
 		{:else}
 			<div class="flex-center text-center">
