99e1e82ce3edaaa0ce9b4a07b86a54c14ed62f95
Swoopy <swoopy@smartworks.studio>
2025-11-06 19:43:00 +0100

    - folder item

--- src/lib/components/FolderItem.svelte
+++ src/lib/components/FolderItem.svelte
@@ -4,14 +4,12 @@
 </script>
 
 <div class="folder-item">
-		<div class="flex-row-center gap-1 mb-2">
-			<div class="folder-icon">
-				<a href="{base}/folders/content" aria-label="folder-icon">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 80" class="m-folder-icon folder-yellow">
-						<path id="folder" d="M90,0a10,10,0,0,1,10,9.9V70a9.94,9.94,0,0,1-9.9,10H10A9.94,9.94,0,0,1,0,70.1V20A9.94,9.94,0,0,1,9.9,10H50L60,0Z"></path>
-					</svg>
-				</a>
-			</div>
+		<div class="folder-icon-container flex-row-center gap-1 mb-2">
+			<a href="{base}/folders/content" class="folder-icon" aria-label="folder-icon">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 80" class="m-folder-icon folder-yellow">
+					<path id="folder" d="M90,0a10,10,0,0,1,10,9.9V70a9.94,9.94,0,0,1-9.9,10H10A9.94,9.94,0,0,1,0,70.1V20A9.94,9.94,0,0,1,9.9,10H50L60,0Z"></path>
+				</svg>
+			</a>
 			<div class="folder-tools">
 				<ControlIcon icon="pencil" title="Edit Folder" data-bs-toggle="modal" data-bs-target="#editFolderModal" />
 				<!-- ControlIcon icon="trash" title="Delete Folder" /-->
